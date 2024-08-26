bcb93c07f4e0705511e2214433c885b9a60d288a
Ishan Garg <ishan.garg10394@gmail.com>
2024-08-26 16:43:11 -0400

    DBLite: Get_page function that gets the page from the file

--- constants.h
+++ constants.h
@@ -7,5 +7,7 @@
 
 #define TABLE_MAX_PAGES 100
 
+const uint32_t PAGE_SIZE = 4096;
+
 
 #endif //DBLITE_CONSTANTS_H
--- pager.h
+++ pager.h
@@ -47,4 +47,36 @@ Pager* pager_open(const char* filename){
 
 }
 
+void* get_page(Pager* pager, uint32_t page_num){
+
+    if (page_num > TABLE_MAX_PAGES){
+        cout << "Page Number Out of Bounds\n";
+        exit(EXIT_FAILURE);
+    }
+
+    if(pager->pages[page_num] == nullptr){
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length/PAGE_SIZE;
+
+        if(pager->file_length%PAGE_SIZE){
+            num_pages += 1;
+        }
+
+        if (page_num <= num_pages){
+            lseek(pager->file_descriptor, page_num*PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+            if (bytes_read == -1){
+                cout << "Error Reading File\n";
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pager->pages[page_num] = page;
+
+    }
+
+    return pager->pages[page_num];
+
+}
+
 #endif //DBLITE_PAGER_H
